12a4e356327b3a1c2ba71a6d11835a769ef50001
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2011-02-10 14:56:49 +0000

    added zen test and devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseCancan::Application.config.secret_token = 'd9a4da34dab8d64ccefa38384550928a04e19fcdaef27068a02bc93adee114e7a65b6cf23d7737b8e3dc7bd62c5dd2e74c53266f96d0c3d082822a2f575712e0'
