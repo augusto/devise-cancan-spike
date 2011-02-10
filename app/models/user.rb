b1935288383b94759194b50317b6d26dda7f75b6
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2011-02-10 16:38:59 +0000

    working app with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  # :registerable,
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
